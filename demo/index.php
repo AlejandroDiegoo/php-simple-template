18d2472bc0eeeeb50e7c3903dfe17d1e31e5f0d4
Alejandro <cuentas@alejandrodiego.com>
2017-01-17 12:05:30 +0100

    Update index.php

--- demo/index.php
+++ demo/index.php
@@ -4,14 +4,14 @@
 
 	$template->add(array(
 		'LIST_TITLE' => 'list title',
-    'LIST_ERROR' => 'list error'
+		'LIST_ERROR' => 'list error'
 	));
 
 	for ($i = 1; $i <= 10; $i++) {
 
 		$template->add(array(
 			'ATTR_1' => 'item ' . $i . ' attr 1',
-      'ATTR_2' => 'item ' . $i . ' attr 2'
+			'ATTR_2' => 'item ' . $i . ' attr 2'
 		), 'LIST_NAME');
 
 	}
